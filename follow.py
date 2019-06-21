6a18a8e8b192403e3bedc311e763a869b46054ff
Graver75 <undergraver75@gmail.com>
2019-06-21 19:36:18 +0300

    adding feed list

--- follow.py
+++ follow.py
@@ -17,11 +17,35 @@ def login(response):
     options = Options()
     options.headless = True
     driver = webdriver.Firefox()
-    # driver = webdriver.Firefox(options=options, executable_path=r'/usr/local/bin/geckodriver')
+    # driver = webdriver.Firefox(options=options, executable_path=r'/usr/local/bin/geckodriver') #headless version, disabled while developing
     driver.get(response.url)
-    time.sleep(5) # loading page and getting session cookie
-    print(driver.get_cookies())
-    return driver
+    time.sleep(10) # loading page and getting session cookie
+    follow(driver)
+
+
+def follow(driver):
+    urls_to_follow = CONFIG['urls']['follow']
+    for elem in urls_to_follow:
+        driver.get(elem["value"])
+        time.sleep(10)
+        feeds = driver.find_elements_by_css_selector('.DiscoverFeed')
+        for feed in feeds:
+            if feed.find_element_by_css_selector('.item-header').text == elem['title']:
+                button = feed.find_element_by_css_selector('.follow')
+                button.click()
+
+                input = driver.find_elements_by_css_selector('.fx-input')[1].find_element_by_tag_name('input')
+                input.send_keys(elem['title'])
+
+                send_div = driver.find_element_by_css_selector('.menu-footer')
+                send_div.click()
+
+                menu = driver.find_elements_by_css_selector('.menu')[1]
+                create_div = menu.find_element_by_css_selector('.primary')
+                create_div.click()
+
+
+
 
 
 process.crawl(get_login_class("mad_device@gravmail.com", "JzscyYVz", login))
