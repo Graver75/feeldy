cfb99215fc54f1f89cb3a9c66f2b8534da2bbeb6
Graver75 <undergraver75@gmail.com>
2019-06-25 16:35:04 +0300

    infinity register with timeout

--- reg_app.py
+++ reg_app.py
@@ -1,10 +1,24 @@
 from RegisterSpider import RegisterSpider
 from scrapy.crawler import CrawlerProcess
+import time, json
+
+with open('config.json') as f:
+    TIMEOUT = json.load(f)["register_timeout"]
 
 
-process = CrawlerProcess({
-    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-})
 
-process.crawl(RegisterSpider)
-process.start()
+def register(temp=None):
+    process = CrawlerProcess({
+        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+    })
+
+    d = process.crawl(RegisterSpider)
+    d.addCallback(after_register)
+    process.start(stop_after_crawl=False)
+
+
+def after_register(temp=None):
+    time.sleep(TIMEOUT)
+    register()
+
+register()
